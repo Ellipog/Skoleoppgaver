3fca8bdbb782d10f1a0e3d352b0c3d713b8d033b
Ellipog <45790498+Ellipog@users.noreply.github.com>
2022-12-13 14:53:37 +0100

    oppgave 1

---
+++ Basic Programmering Intro Oppgaver/js/oppgave1.js
@@ -0,0 +1,19 @@
+function testA() {
+    let testA = 8;
+    document.getElementById("testA").innerHTML = testA;
+}
+
+function testB() {
+    let testB = "testverdi";
+    document.getElementById("testB").innerHTML = testB;
+}
+
+function testC() {
+    let testC = 2 * 3;
+    document.getElementById("testC").innerHTML = testC;
+}
+
+function testD() {
+    let testD = 2 / 3;
+    document.getElementById("testD").innerHTML = testD;
+}
