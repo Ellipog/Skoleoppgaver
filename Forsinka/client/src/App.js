467496f30efe705d43892f7668d8a49415fcb2ad
Ellipog <elliotsaaen@hotmail.com>
2023-01-23 18:12:41 +0100

    gh

--- Forsinka/client/src/App.js
+++ Forsinka/client/src/App.js
@@ -3,39 +3,34 @@ import React, { useState, useEffect } from "react";
 import Forsinkelse from "./components/Forsinkelse";
 
 function App() {
-    const [forsinkelser, setForsinkelser] = useState([]);
+  const [forsinkelser, setForsinkelser] = useState([]);
 
-    async function test() {
-        fetch("httpsn://node.binders.net:25592/forsinkelser")
-            .then((res) => res.json())
-            .then((data) => {
-                data = data.sort((a, b) => {
-                    return new Date(b.aimedTime) - new Date(a.aimedTime);
-                });
-                setForsinkelser(data);
-                console.log("Fetching");
-            });
-    }
+  async function test() {
+    fetch("https://node.binders.net:25592/forsinkelser")
+      .then((res) => res.json())
+      .then((data) => {
+        data = data.sort((a, b) => {
+          return new Date(b.aimedTime) - new Date(a.aimedTime);
+        });
+        setForsinkelser(data);
+        console.log("Fetching");
+      });
+  }
 
-    useEffect(() => {
-        test();
-    }, []);
+  useEffect(() => {
+    test();
+  }, []);
 
-    return ( 
-    <div className="App" >
-        <div className="header" >
-        <input className="search" type="text" ></input>
-        </div> {
-            forsinkelser.map((forsinkelse, i) => {
-                return <Forsinkelse key = { i }
-                line={forsinkelse.line}
-                expectedTime={forsinkelse.expectedTime}
-                aimedTime={forsinkelse.aimedTime}
-                name={forsinkelse.name}
-                />;
-            })
-        } </div>
-    );
+  return (
+    <div className="App">
+      <div className="header">
+        <input className="search" type="text"></input>
+      </div>{" "}
+      {forsinkelser.map((forsinkelse, i) => {
+        return <Forsinkelse key={i} line={forsinkelse.line} expectedTime={forsinkelse.expectedTime} aimedTime={forsinkelse.aimedTime} name={forsinkelse.name} />;
+      })}{" "}
+    </div>
+  );
 }
 
 export default App;
