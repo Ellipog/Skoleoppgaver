7267ac8778b913ea33690e53fdc091bb6a40684e
Ellipog <45790498+Ellipog@users.noreply.github.com>
2023-01-26 13:19:08 +0100

    Inital Commit

---
+++ Svelte Intro/src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+    let lengde = 100
+    let bredde = 80
+    $: areal = lengde*bredde/1000
+</script>
+
+<div class="box">
+    <label>Lende: <input type="range" bind:value={lengde}> cm</label>
+    <label>Bredde: <input type="range" bind:value={bredde}> cm</label>
+    <div class="answer" style="width: {lengde}px; height: {bredde}px;">
+        Gulvarealet er {areal} m<sup>2</sup>
+    </div>
+</div>
+
+<style>
+    .answer {
+        background-color: brown;
+        color: white;
+        text-align: center;
+    }
+
+    .box {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    }
+</style>
